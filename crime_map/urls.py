bf5cea70234f058c8b4868ad8482b621eccf297d
Tolamo Butale <tolamografesx@gmail.com>
2025-07-18 12:53:20 +0200

    add

--- crime_map/urls.py
+++ crime_map/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import map_view, pin_drop, quick_pin, hotzone_detail, pin_drop_location
+from . import views
 
 urlpatterns = [
     path('', map_view, name='crime_map'),
@@ -7,4 +8,5 @@ urlpatterns = [
     path('quick-report/', quick_pin, name='quick_pin'),
     path('hotzone/<int:pk>/', hotzone_detail, name='hotzone_detail'),
     path('pin-drop-location/', pin_drop_location, name='pin_drop_location'),
+    path('pin-drop-location/', views.pin_drop_location, name='pin_drop_location'),
 ]
